d2f42167d639c9d7c0657366c14e5837e8cb231b
Ryan Trang <rtrang16@gmail.com>
2017-05-19 05:54:07 +0000

    initial commit

---
+++ app/api/url-shortener.js
@@ -0,0 +1,68 @@
+module.exports = function(app, db) {
+    
+    app.route('/:url')
+        // Check & retrieve url to redirect if it exists.
+        .get(handleGet);
+    
+    app.get('/new/:url*', handlePost);
+    
+    function handleGet(req, res) {
+        var url = process.env.APP_URL + req.params.url;
+        if (url != process.env.APP_URL + 'favicon.ico') {
+            findURL(url, db, res);
+        }
+    }
+    
+    function handlePost(req, res) {
+        // Create short url, store & display the info
+        var url = req.url.slice(5);
+        var urlObj = {};
+        if (validateURL(url)) {
+            urlObj = {
+                "original_url": url,
+                "short_url": process.env.APP_URL + linkGen()
+            };
+            res.send(urlObj);
+            save(urlObj, db);
+        } else {
+            urlObj = {
+                "error": "No short url found for given input"
+            };
+            res.send(urlObj);
+        }
+    }
+    
+    function linkGen() {
+        // Generates random four digit number for link
+        var num = Math.floor(100000 + Math.random() * 900000);
+        return num.toString().substring(0, 4);
+    }
+    
+    function findURL(link, db, res) {
+        // Check to see if the site is already there
+        var sites = db.collection('sites');
+        // Get the URL
+        sites.findOne({
+            "short_url": link
+        }, function(err, result) {
+            if (err) throw err;
+            // Object of the URL
+            if (result) {
+                // We have a result
+                console.log('Found ' + result);
+                console.log('Redirecting to: ' + result.original_url);
+                res.redirect(result.original_url);
+            } else {
+                // We don't
+                res.send('Site not found');
+            }
+        });
+    }
+    
+    function validateURL(url) {
+        // Checks to see if it is an actual URL
+        var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+        return regex.test(url);
+    }
+};
+
